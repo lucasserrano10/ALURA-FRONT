ac79f8b25cad3ac0da353a3f4f44d1193bbd6d9f
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-08-08 19:20:32 -0300

    finalizando projeto

--- aluramidi-curso-arquivos-iniciais/app.js
+++ aluramidi-curso-arquivos-iniciais/app.js
@@ -1,12 +1,30 @@
 const listaTeclas = document.querySelectorAll('.tecla')
 
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio)
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play()
+    }
+    else{
+        alert('elemento não encontrado ou seletor não encontrado')
+    }
 }
 
-let i = 0
+for(let i = 0; i < listaTeclas.length ;i += 1){
+    const tecla = listaTeclas[i]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    };
 
-while(i < 9){
-    listaTeclas[i].onclick = tocaSomPom;
-    i += 1
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
 }
